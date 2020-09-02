e564aa527d2c444c85f705ae4b4f6dcdfc142416
PabloPolito <pablopietropolito@gmail.com>
2020-09-01 22:04:05 -0300

    Commit realizado em todas as pastas, subpastas e arquivos do projeto Faculdade

---
+++ Subjects/Level_1_Programming/Desafio_Java.java
@@ -0,0 +1,56 @@
+class ContaBancaria {
+	public String titular;
+	public String email;
+	public double saldo;
+	 
+	    public void saque(double valor ) {
+            if (saldo >= valor ) {
+                System.out.println("retirada  " + valor);
+                saldo = saldo - valor;
+            }
+            else {
+                System.out.println("saldo insuficiente para a retirada de  " + valor);
+		    }
+        }
+        public void deposito(double valor2 ) {
+            if (valor2 >= 0  ) {
+                System.out.println(" Deposito de:  " + valor2);
+                saldo = saldo + valor2;
+            }
+            else {
+                System.out.println("Voce nao pode depositar o valor de:   " + valor2);
+		    }
+        }
+
+
+    public String extratoSimples() {
+		 return "\n Correntista:  " + titular + "\n Valor atual da conta:   " + saldo + "\n Email: " + email;
+	    }
+}
+
+class ProgramaAgencia {
+    public static void main(String args[]) {
+
+        ContaBancaria contaUm = new ContaBancaria();
+            contaUm.titular = "Eliane";
+            contaUm.saldo = 100;
+            System.out.println(contaUm.extratoSimples());
+            
+        ContaBancaria contaDois = new ContaBancaria();
+            contaDois.titular = "Henrique";
+            contaDois.saldo = 600;
+            System.out.println(contaDois.extratoSimples());
+            
+        ContaBancaria contaTres = new ContaBancaria();
+            contaTres.titular = "Pablo";
+            contaTres.saldo = 15000;
+            contaTres.email = "pablopietropolito@gmail.com";
+            System.out.println(contaTres.extratoSimples()); 
+ 
+            System.out.println("");
+            
+            contaTres.deposito(1000);
+            System.out.println(contaTres.extratoSimples());
+
+    }
+}
---
+++ Subjects/Level_1_Programming/Desafio_Java_2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+
+class Livro {
+public String titulo;
+public String autor;
+public int numeroPaginas;
+	
+public String exibirLivro(){
+ return "\nLivro: " + titulo + "\nEscritor: " + autor + "\nNumero de paginas: " + numeroPaginas;
+}}
+
+class CriarLivros{
+	public static void main (String args[])  {
+	
+	Scanner entradaDados = new Scanner( System.in );
+	Livro livroUm = new Livro();
+	
+	System.out.print( "Digite o titulo do livro:  "); 
+	livroUm.titulo = entradaDados.nextLine(); 
+
+	System.out.print( "Digite o nome do autor:  "); 
+	livroUm.autor = entradaDados.nextLine(); 
+	
+	System.out.print( "Digite o numero de paginas:  "); 
+	livroUm.numeroPaginas = entradaDados.nextInt();
+ 
+    System.out.println(livroUm.exibirLivro());
+
+    entradaDados.close();
+}}
